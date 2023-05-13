e05fa23fd2fa3da7e353d4184b3f57a9c62b6d40
Sérgio Bueno <84647000+Coastony@users.noreply.github.com>
2023-05-13 13:06:22 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,38 @@
-const input = document.querySelector('input');
-const search = document.querySelector('img.search');
+//Input and Search
+const input = document.querySelector("input");
+const search = document.querySelector("img.search");
 
+input.addEventListener("keypress", (event) => {
+  event.preventDefault;
+  const keyName = event.key;
 
-input.addEventListener('keypress', event => {
-    event.preventDefault;
-    const keyName = event.key;
-
-    if(keyName == 'Enter') {
-        location.reload();
-    }
-})
-
-search.addEventListener('click', event => {
+  if (keyName == "Enter") {
     location.reload();
-})
-
-let show = true;
-
-const menuSection = document.querySelector(".menu-section");
-const menuToggle = menuSection.querySelector(".menu-toggle");
-
-menuToggle.addEventListener("click", () => {
-
- document.body.style.overflow = show ? "hidden" : "initial";
- menuSection.classList.toggle("on", show);
- show = ! show;
+  }
 });
+
+search.addEventListener("click", (event) => {
+  location.reload();
+});
+
+const btnMobile = document.getElementById("btn-mobile");
+
+// Toggle nav
+function toggleMenu(event) {
+  if (event.type === "touchstart") event.preventDefault();
+
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("active");
+  const active = nav.classList.contains("active");
+
+  event.currentTarget.setAttribute("aria-expanded", active);
+
+  if (active) {
+    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+  }
+}
+
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
